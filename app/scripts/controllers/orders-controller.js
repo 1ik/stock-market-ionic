a4f9dad9a81561a7547c9fc576455c948c121244
anik <=>
2015-09-28 10:26:28 +0600

    order search with company filter

--- app/scripts/controllers/orders-controller.js
+++ app/scripts/controllers/orders-controller.js
@@ -1,4 +1,30 @@
 angular.module('app.controllers').controller(
-	'OrdersController', ['$scope',function($scope){
+	'OrdersController', ['$scope', 'ordersService', function($scope, ordersService){
+		$scope.companies = ["select"];
 		
+		$scope.search = {
+			company: "select"
+		};
+
+		ordersService.getOrders({}).then(function(orders){
+			$scope.orders = orders;
+			_.each(orders, function(order){
+				if($scope.companies.indexOf(order.company) == -1) {
+					$scope.companies.push(order.company);
+				}
+			})
+		});
+
+		$scope.$watch('search.company', function(val){
+			if(val != 'select') {
+				doSearch();
+			}
+		});
+
+		var doSearch = function() {
+			ordersService.getOrders($scope.search).then(function(orders){
+				$scope.orders = orders;
+			});
+		}
+
 }]);
---
+++ app/scripts/services/orders-service.js
@@ -0,0 +1,12 @@
+angular.module('app.services')
+
+.service('ordersService', ['httpUtils', "constants", '$localstorage', function(httpUtils, constants, $localstorage){
+	
+	return {
+		getOrders : function(query) {
+			var params = $.param(query);
+			var reqUrl = constants.rootURL + 'api_orders/orders?' + params;
+			return httpUtils.get(reqUrl, {}, {'Token':$localstorage.getObject('userData').token}).promise;
+		}
+	};
+}])
