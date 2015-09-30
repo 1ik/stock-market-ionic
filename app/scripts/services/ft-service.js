7b5013c98d763e3da8f0fd1ca513325a244da51c
anik <=>
2015-09-30 11:57:40 +0600

    FT save

--- app/scripts/services/ft-service.js
+++ app/scripts/services/ft-service.js
@@ -6,6 +6,13 @@ angular.module('app.services')
 		getFTRequests: function() {
 			var reqUrl = constants.rootURL + 'api_fundtransfer/status';
 			return httpUtils.get(reqUrl, {}, {'Token':$localstorage.getObject('userData').token}).promise;
-		}
+		},
+
+		sendFTReq: function(data) {
+			console.log(data);
+			var reqUrl = constants.rootURL + 'api_fundtransfer/saveRequest';
+			var params = $.param(data);
+			return httpUtils.post(reqUrl, params, {'Token':$localstorage.getObject('userData').token}).promise;
+		},
 	};
 }])
